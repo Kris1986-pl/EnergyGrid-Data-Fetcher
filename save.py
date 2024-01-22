4b31fbbc8bd3da347e0314f8e6103732f9a6e18b
Kris <krzysztof.kinel86@gmail.com>
2024-01-22 12:32:45 +0100

    Implemeted Insert Intra day

--- save.py
+++ save.py
@@ -1,37 +1,67 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import sqlite3
+
+import numpy as np
+import pandas as pd
 
 from database import Database
 from fetcher import DataFetcherFactory
 
 
-def fetch_day_ahead(date):
+def fetch_data(date: datetime, name: str) -> DataFetcherFactory:
     try:
         # Create a Day-Ahead data fetcher
         data_fetcher_factory = DataFetcherFactory()
-        fetcher = data_fetcher_factory.create_data_fetcher("Day-Ahead", date)
+        fetcher = data_fetcher_factory.create_data_fetcher(name, date)
         return fetcher.fetch_data()
     except ValueError as ve:
         # Handle other ValueErrors
         print(f"Error: {ve}")
 
 
-def insert_date(DB, df):
+def insert_date(db: Database, data: pd.DataFrame):
+    query = insert_date_query = f"INSERT INTO date (date_value) " \
+                                f"VALUES ('{df_da.index[0].strftime('%Y-%m-%d')}')"
     try:
-        DB.insert_data(insert_date_query)
+        db.insert_data(query)
+        print(f"Date {data.index[0].strftime('%Y-%m-%d')} save correctly")
     except sqlite3.IntegrityError:
-        print(f"Date: {df.index[0].strftime('%Y-%m-%d')} already exist in Date table")
+        print(f"Date: {data.index[0].strftime('%Y-%m-%d')} already exist in Date table")
 
 
-def insert_day_ahead(db, df, select_date_query):
-    date = db.select_data(select_date_query)
+def insert_day_ahead(db: Database, data: pd.DataFrame):
+    query = f"SELECT date_id " \
+            f"FROM date " \
+            f"WHERE date_value = '{df_da.index[0].strftime('%Y-%m-%d')}'"
+    date = db.select_data(query)
     date_id = date[0][0]
     try:
-        for index, row in df.iterrows():
-            insert_query = f"INSERT INTO day_ahead (hour_of_day, date_id, price) VALUES ('{row['hour']}', {date_id}, {row['price']})"
+        for index, row in data.iterrows():
+            insert_query = f"INSERT INTO day_ahead (hour_of_day, date_id, price) " \
+                           f"VALUES ('{row['hour']}', {date_id}, {row['price']})"
             db.insert_data(insert_query)
+        print("Data from Day Ahead saved correctly.")
     except sqlite3.IntegrityError:
-        print(f"Date: {df.index[0].strftime('%Y-%m-%d')} already exist in Day Ahead table")
+        print(f"Date: {data.index[0].strftime('%Y-%m-%d')} already exist in Day Ahead table")
+
+def insert_intra(db: Database, data: pd.DataFrame):
+    query = f"SELECT date_id " \
+            f"FROM date " \
+            f"WHERE date_value = '{data.index[0].strftime('%Y-%m-%d')}'"
+    date = db.select_data(query)
+    date_id = date[0][0]
+    print(data)
+    data.replace('-', "Null", inplace=True)
+    data.replace(np.nan, "Null", inplace=True)
+    try:
+        for index, row in data.iterrows():
+            insert_query = f"INSERT INTO intra_day (hour_of_day, date_id, intraday_avg_price, intraday_min_price, intraday_max_price) " \
+                           f"VALUES ('{row['hour']}', {date_id}, {row['cenaIntraAvg']}, {row['cenaIntraMin']}, {row['cenaIntraMax']})"
+            print(insert_query)
+            DB.insert_data(insert_query)
+        print("Data from Intra Day saved correctly.")
+    except sqlite3.IntegrityError:
+        print(f"Date: {df_intra.index[0].strftime('%Y-%m-%d')} already exist in Intra Day table")
 
 
 if __name__ == "__main__":
@@ -39,9 +69,14 @@ if __name__ == "__main__":
     DB = Database("energy.db")
     SQLITE_PATH = 'energy.db'
 
-    df = fetch_day_ahead(DATE)
-    insert_date_query = f"INSERT INTO date (date_value) VALUES ('{df.index[0].strftime('%Y-%m-%d')}')"
-    insert_date(DB, df)
+    # Day Ahead
+    # Save date which was fetched from Day Ahead
+    df_da = fetch_data(DATE, "Day-Ahead")
+    insert_date(DB, df_da)
+    # Save date which was fetched from Day Ahead
+    insert_day_ahead(DB, df_da)
 
-    select_date_query = f"SELECT date_id FROM date WHERE date_value = '{df.index[0].strftime('%Y-%m-%d')}'"
-    insert_day_ahead(DB, df, select_date_query)
+    # Intra Day
+    # Save date which was fetched from Intra Day
+    df_intra = fetch_data(DATE, "Intra-Day")
+    insert_intra(DB, df_intra)
