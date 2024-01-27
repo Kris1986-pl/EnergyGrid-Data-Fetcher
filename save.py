d7fd65e9a7dad72c8cd1c30afff4ae10a374d4d8
Kris1986-pl <krzysztof.kinel86@gmail.com>
2024-01-27 20:47:54 +0100

    Implemeted insert_pse_current function

--- save.py
+++ save.py
@@ -44,7 +44,7 @@ def insert_date(db: Database, data: pd.DataFrame):
    """
     query = insert_date_query = f"INSERT INTO date (date_value) " \
                                 f"VALUES ('{data.index[0].strftime('%Y-%m-%d')}')"
-    print(query)
+
     try:
         db.insert_data(query)
         print(f"Date {data.index[0].strftime('%Y-%m-%d')} save correctly")
@@ -171,7 +171,8 @@ def insert_pse_5(db: Database, data: pd.DataFrame):
             DB.insert_data(insert_query)
         print("Data from PSE five-years plan saved correctly.")
     except sqlite3.IntegrityError:
-        print(f"Date: {data.index[0].strftime('%Y-%m-%d')} already exist in PSE five-years plan table")
+        print(
+            f"Date: {data.index[0].strftime('%Y-%m-%d')} already exist in PSE five-years plan table")
 
 
 def insert_pse_bal(db: Database, data: pd.DataFrame):
@@ -205,7 +206,66 @@ def insert_pse_bal(db: Database, data: pd.DataFrame):
 
 
 def insert_pse_current(db: Database, data: pd.DataFrame):
-    pass
+    query = f"SELECT date_id " \
+            f"FROM date " \
+            f"WHERE date_value = '{data.index[0].strftime('%Y-%m-%d')}'"
+    date = db.select_data(query)
+    date_id = date[0][0]
+    data.replace('-', "Null", inplace=True)
+    data.replace(np.nan, "Null", inplace=True)
+    # print(data.columns)
+    try:
+        for index, row in data.iterrows():
+            insert_query = f"INSERT OR REPLACE INTO current_daily_plan " \
+                           f"(hour_of_day, " \
+                           f"date_id, " \
+                           f"NationalPowerDemand," \
+                           f"TotalProductionCapacity_KSE," \
+                           f"TotalProductionCapacity_JGWa," \
+                           f"TotalProductionCapacity_JGFWa," \
+                           f"TotalProductionCapacity_JGMa," \
+                           f"TotalProductionCapacity_JGPVa," \
+                           f"TotalGeneration_ActiveJG," \
+                           f"TotalGeneration_JGWa," \
+                           f"TotalGeneration_JGFWa," \
+                           f"TotalGeneration_JGMa," \
+                           f"TotalGeneration_JGPVa," \
+                           f"TotalGeneration_NonParticipatingUnits," \
+                           f"WindPowerGeneration," \
+                           f"PVPowerGeneration," \
+                           f"TotalChargingCapacity_JGMa," \
+                           f"NationalParallelExchangeBalance," \
+                           f"NationalNonParallelExchangeBalance," \
+                           f"ExcessCapacityAboveDemand," \
+                           f"ExcessCapacityBelowDemand," \
+                           f"TotalCapacityFromUtilizedLoadReductionOffers_JGOa)" \
+                           f"VALUES ({row['Godzina']}, " \
+                           f"{date_id}, " \
+                           f"{row['Krajowe zapotrzebowanie na moc']}, " \
+                           f"{row['Suma zdolnoci wytw๓rczych jednostek wytw๓rczych w KSE']}, " \
+                           f"{row['Suma zdolnoci wytw๓rczych JGWa']}, " \
+                           f"{row['Suma zdolnoci wytw๓rczych JGFWa']}, " \
+                           f"{row['Suma zdolnoci wytw๓rczych JGMa']}, " \
+                           f"{row['Suma zdolnoci wytw๓rczych JGPVa']}, " \
+                           f"{row['Sumaryczna generacja JG aktywnych: JGWa, JGFWa, JGMa i JGPVa']}, " \
+                           f"{row['Sumaryczna generacja JGWa']}," \
+                           f"{row['Sumaryczna generacja JGFWa']}, " \
+                           f"{row['Sumaryczna generacja JGMa']}, " \
+                           f"{row['Sumaryczna generacja JGPVa']}, " \
+                           f"{row['Sumaryczna generacja jednostek wytw๓rczych nieuczestniczนcych aktywnie w Rynku Bilansujนcym']}, " \
+                           f"{row['Generacja r๓deณ wiatrowych']}, " \
+                           f"{row['Generacja r๓deณ fotowoltaicznych']}, " \
+                           f"{row['Sumaryczna moc ณadowania JGMa']}, " \
+                           f"{row['Krajowe saldo wymiany mi๊dzysystemowej r๓wnolegณej']}, " \
+                           f"{row['Krajowe saldo wymiany mi๊dzysystemowej nier๓wnolegณej']}, " \
+                           f"{row['Rezerwa mocy ponad zapotrzebowanie']}, " \
+                           f"{row['Rezerwa mocy poniฟej zapotrzebowania']}, " \
+                           f"{row['Suma mocy z wykorzystanych Ofert Redukcji Obciนฟenia JGOa']})"
+            # print(insert_query)
+            DB.insert_data(insert_query)
+        print("Data from PSE Current Daily Coordination Plan saved correctly.")
+    except sqlite3.DatabaseError as e:
+        print(e)
 
 
 if __name__ == "__main__":
@@ -245,10 +305,10 @@ if __name__ == "__main__":
     # Save data which was fetched from PSE Balancing Market
     insert_pse_bal(DB, df_bal)
 
-    # PSE PSE Balancing Market
+    # PSE Current Daily
     print("PSE Current Daily Coordination Plan is being fetched...")
     df_curr = fetch_data(DATE, "PSE Current Daily Coordination Plan")
     # Save date which was fetched from PSE Balancing Market
     insert_date(DB, df_curr)
     # Save data which was fetched from PSE Balancing Market
-    # insert_pse_bal(DB, df_curr)
+    insert_pse_current(DB, df_curr)
